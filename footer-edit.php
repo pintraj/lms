b20bea6d65651fd1a4233c31f62e6c4a32587def
pintraj <pint.raj850@gmail.com>
2021-01-14 15:12:34 +0530

    footer edit

---
+++ footer-edit.php
@@ -0,0 +1,9 @@
+<?php
+?>
+<!--bootstrap -->
+<script src="js/bootstrap/bootstrap.js"></script>
+<script src="js/bootstrap/bootstrap.esm.js"></script>
+<script src="js/bootstrap/bootstrap.bundle.js"></script>
+
+<!--custom js-->
+<script src="js/custom.js"></script>
